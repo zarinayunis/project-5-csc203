507118347f74d8a8ec7ae78b9ecb3ee40e5c2999
Zarina <zarinayunis@gmail.com>
2023-03-21 14:18:02 -0700

    commiting

---
+++ src/Cat.java
@@ -0,0 +1,41 @@
+import processing.core.PImage;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cat extends LiveObj{
+    public Cat(String id, Point position, List<PImage> images, int resourceLimit, int resourceCount, double actionPeriod, double animationPeriod, int health, int healthLimit){
+        super(id, position, images, resourceLimit, resourceCount, actionPeriod, animationPeriod, health, healthLimit);
+    }
+    public boolean moveTo(WorldModel world, Entity rat, EventScheduler scheduler){
+        if (this.position.adjacent(rat.position)) {
+            world.removeEntity(scheduler, rat);
+            return true;
+        } else {
+            Point nextPos = this.nextPosition(world, rat.position);
+
+            if (!this.position.equals(nextPos)) {
+                world.moveEntity(scheduler, this, nextPos);
+            }
+            return false;
+        }
+    }
+    public Point nextPosition(WorldModel world, Point destPos) {
+        AStarPathingStrategy pathingStrategy = new AStarPathingStrategy();
+        List<Point> path = pathingStrategy.computePath(position, destPos,
+                p -> world.withinBounds(p) && !world.isOccupied(p),
+                Point::adjacent,
+                PathingStrategy.CARDINAL_NEIGHBORS);
+
+        if (path.size() >= 1) {
+            return path.get(0);
+        } else {
+            return position;
+        }
+    }
+    public void executeActivity(WorldModel world, ImageStore imageStore, EventScheduler scheduler){
+        List<Entity> targetEntities = new ArrayList<>();
+        targetEntities.add(new Rat(null, null, null, 0, 0, 0, 0, 0, 0));
+    }
+
+}
